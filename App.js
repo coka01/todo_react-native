974fe08dc12d5b41f5afbe426935902a354db834
KobayashiKz <anuuihsa9711212@gmail.com>
2018-12-16 19:43:03 +0900

    TODO保存処理/読み出し処理の追加
    ・TODO保存処理
    ・TODO読み出し処理
    ・コメントの追加

--- App.js
+++ App.js
@@ -8,7 +8,7 @@
 
 import React, {Component} from 'react';
 import {Platform, StyleSheet, Text, View, TextInput,
-  TouchableOpacity, ScrollView} from 'react-native';
+  TouchableOpacity, ScrollView, AsyncStorage} from 'react-native';
 import TodoList from './TodoList';
 
 const instructions = Platform.select({
@@ -26,23 +26,51 @@ export default class App extends Component<Props> {
     todos: [],
   }
 
+  // コンストラクタ
+  constructor(props) {
+    super(props);
+    this.loadTodos();
+  }
+
+  // テキストボックスのテキストが変更されるたびに呼ばれる処理
   onChangeText(newTodo) {
     this.setState({ newTodo });
   }
 
+  // Addボタンタップ時の処理
   onPressAdd() {
     const { newTodo } = this.state;
+    // setStateはすぐに反映されないため、setState()直後にstateを参照しても古い情報が返ってくる
+    // 第二引数にcallbackを設定できるため、そこでデータ保存処理を行う
     this.setState ({
       newTodo: '',
       todos: [newTodo, ...this.state.todos],
-    })
+    }, () => this.storeTodos());
   }
 
+  // 削除ボタンタップ時の処理
   onPressDelete(index) {
     this.setState({
       // index番目の配列を抜いた配列を生成する
       todos: this.state.todos.filter((t, i) => i !== index),
-    });
+    }, () => this.storeTodos());
+  }
+
+  // Todoの保存処理
+  storeTodos() {
+    // String配列をStringに変換する
+    const str = JSON.stringify(this.state.todos);
+    AsyncStorage.setItem('todos', str);
+  }
+
+  // 保存したTodoの読み出し処理
+  loadTodos() {
+    // 非同期で取得. 第二引数にはcallbackを設定できる.
+    AsyncStorage.getItem('todos').then((str) => {
+      // 値が取得できたらString配列に戻す
+      const todos = str ? JSON.parse(str) : [];
+      this.setState({todos});
+    })
   }
 
   render() {
